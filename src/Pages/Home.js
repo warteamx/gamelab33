6dcf8aa6f2be2bec4820a63793648aecbf82c6e9
martinrebo <martinrg33@gmail.com>
2019-03-12 12:46:37 +0100

    BoilerPlate Start

---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AtomSpinner from '@bit/bondz.react-epic-spinners.atom-spinner';
+
+function Home() {
+    return (
+        <>
+        	<AtomSpinner
+	color='#000000'
+	size='200'
+	/>
+        </>
+    )
+}
+
+export default Home;
