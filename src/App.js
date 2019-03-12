0fb14d66515b1ce508876589368fbaa5039b4946
martinrebo <martinrg33@gmail.com>
2019-03-12 13:39:22 +0100

    add home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './Pages/Home'
+import Home from './Pages/Home';
 
 class App extends Component {
   render() {
