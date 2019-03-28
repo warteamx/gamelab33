2705acfc783ec507e9c87fad9f478dd4ebf55c81
martinrebo <martinrg33@gmail.com>
2019-03-28 07:56:58 +0100

    Test enviroment  Not working
    Failing script to npm run start /

--- src/Components/Card/CardResearch.js
+++ src/Components/Card/CardResearch.js
@@ -7,9 +7,11 @@ export default function CardResearch(props) {
         <img src={props.src} alt=""></img>
         <h3>{props.title}</h3>
         <p> {props.description}</p>
+        <p> {props.duration}</p>
         <p>{props.type}</p>
         <p> {props.tags}</p>
         <p> {props.author}</p>
+        <a href={props.link} target="_blank" > Link </a>
 
         </div>
     ) 
--- src/Research/Research1.js
+++ src/Research/Research1.js
@@ -3,7 +3,7 @@ import CardResearch from '../Components/Card/CardResearch';
 import Grid from "@bit/mui-org.material-ui.grid";
 import TopMenu from "../Components/TopMenu/TopMenu";
 import VideoBackground from '../Components/VideoBackground/VideoBackground';
-
+import AddResearchCard from '../Components/AddResearchCard/AddResearchCard';
 
 export default function Research1() {
     return(
@@ -13,17 +13,18 @@ export default function Research1() {
         <TopMenu/>
         <Grid container spacing={8}>
         <Grid item xs={12} sm={4}>
-        <CardResearch title="Card1" description="Description 1" type="type1" tags="tag1 tag2" author="Martin"/>
+        <CardResearch title="Computer Science" description="The OSSU curriculum is a complete education in computer science using online materials. " duration="2 years - 22h/week" type="course" tags="programming, math, theory, " author="Open Source " link="https://github.com/ossu/computer-science"/>
         </Grid>
         <Grid item xs={12} sm={4}>
-        <CardResearch title="Card2" description="Description 2" type="type2" tags="tag1 tag2" author="Anonimo"/>
+        <CardResearch title="Let’s build a full stack MongoDB, React, Node and Express (MERN) app" description="We will build it from nothing. We will setup our own database, create the back end from the ground up and bootstrap our front end with the bare minimum." type="article" tags="javascrip, mongo, express" author="Medium" link="https://medium.com/javascript-in-plain-english/full-stack-mongodb-react-node-js-express-js-in-one-simple-app-6cc8ed6de274"/>
         </Grid>
         <Grid item xs={12} sm={4}>
-        <CardResearch title="Card3" description="Description 3" type="type3" tags="tag1 tag2" author="Other"/>
+        <CardResearch title="React - The Complete Guide (incl Hooks, React Router, Redux)" description="Dive in and learn React from scratch! Learn Reactjs, Hooks, Redux, React Routing, Animations, Next.js and way more!" type="course" duration="39h" tags="React, Redux" author="Other" Link="https://www.udemy.com/react-the-complete-guide-incl-redux/"/>
         </Grid>
-        
+        <AddResearchCard/>
         </Grid>
         <p>It is working</p>
+
         </div>
         </>
     )
