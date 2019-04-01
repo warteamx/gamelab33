af491e865a45df5b4ed665b0edbf9a88cb081b7e
martinrebo <martinrg33@gmail.com>
2019-04-01 12:03:19 +0200

    add research card

--- src/Components/AddResearchCard/AddResearchCard.js
+++ src/Components/AddResearchCard/AddResearchCard.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function AddResearchCard() {
     return (
-        <div>
-        It is working or not
+        <div title="work in progress">
+        + Add Research Card *WIP
         </div>
     );
 }
