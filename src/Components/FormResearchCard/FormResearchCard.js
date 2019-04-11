f020047e0e897e2294e59c51dadf7131fcf8b7e8
martinrebo <martinrg33@gmail.com>
2019-04-11 07:35:05 +0200

    add firebase

--- src/Components/FormResearchCard/FormResearchCard.js
+++ src/Components/FormResearchCard/FormResearchCard.js
@@ -1,163 +1,18 @@
-import React, { Component } from 'react'
-import './FormResearchCard.scss';
-import axios from "axios";
+import React from 'react'
+import firebase from "firebase"
 
-class FromResearchCard extends Component {
-
-  state = {
-    data: [],
-    id: 0,
-    title: null,
-    intervalIsSet: false,
-    idToDelete: null,
-    idToUpdate: null,
-    objectToUpdate: null
+export default function FormResearchCard() {
+  
+  var config = {
+    apiKey: "AIzaSyAsFV6oddVNePcYl-hSuzBf70_ciJw-bfU",
+    authDomain: "lab-33.firebaseapp.com",
+    databaseURL: "https://lab-33.firebaseio.com",
+    storageBucket: "lab-33.appspot.com"
   };
+  firebase.initializeApp(config);
 
-  // when component mounts, first thing it does is fetch all existing data in our db
-  // then we incorporate a polling logic so that we can easily see if our db has 
-  // changed and implement those changes into our UI
-  componentDidMount() {
-    this.getDataFromDb();
-    if (!this.state.intervalIsSet) {
-      let interval = setInterval(this.getDataFromDb, 5000);
-      this.setState({ intervalIsSet: interval });
-    }
-  }
+  return (
+    <div> It it working </div>
 
-  // never let a process live forever 
-  // always kill a process everytime we are done using it
-  componentWillUnmount() {
-    if (this.state.intervalIsSet) {
-      clearInterval(this.state.intervalIsSet);
-      this.setState({ intervalIsSet: null });
-    }
-  }
-
-  // just a note, here, in the front end, we use the id key of our data object 
-  // in order to identify which we want to Update or delete.
-  // for our back end, we use the object id assigned by MongoDB to modify 
-  // data base entries
-
-  // our first get method that uses our backend api to 
-  // fetch data from our data base
-  getDataFromDb = () => {
-    fetch("http://localhost:3001/api/getData")
-      .then(data => data.json())
-      .then(res => this.setState({ data: res.data }));
-  };
-
-  // our put method that uses our backend api
-  // to create new query into our data base
-  putDataToDB = title => {
-    let currentIds = this.state.data.map(data => data.id);
-    let idToBeAdded = 0;
-    while (currentIds.includes(idToBeAdded)) {
-      ++idToBeAdded;
-    }
-
-    axios.post("http://localhost:3001/api/putData", {
-      id: idToBeAdded,
-      title: title
-    });
-  };
-
-
-  // our delete method that uses our backend api 
-  // to remove existing database information
-  deleteFromDB = idTodelete => {
-    let objIdToDelete = null;
-    this.state.data.forEach(dat => {
-      if (dat.id == idTodelete) {
-        objIdToDelete = dat._id;
-      }
-    });
-
-    axios.delete("http://localhost:3001/api/deleteData", {
-      data: {
-        id: objIdToDelete
-      }
-    });
-  };
-
-
-  // our update method that uses our backend api
-  // to overwrite existing data base information
-  updateDB = (idToUpdate, updateToApply) => {
-    let objIdToUpdate = null;
-    this.state.data.forEach(dat => {
-      if (dat.id == idToUpdate) {
-        objIdToUpdate = dat._id;
-      }
-    });
-
-    axios.post("http://localhost:3001/api/updateData", {
-      id: objIdToUpdate,
-      update: { title: updateToApply }
-    });
-  };
-
-
-  // here is our UI
-  // it is easy to understand their functions when you 
-  // see them render into our screen
-  render() {
-    const { data } = this.state;
-    return (
-      <div>
-        <ul>
-          {data.length <= 0
-            ? "NO DB ENTRIES YET"
-            : data.map(dat => (
-                <li style={{ padding: "10px" }} key={data.title}>
-                  <span style={{ color: "gray" }}> id: </span> {dat.id} <br />
-                  <span style={{ color: "gray" }}> data: </span>
-                  {dat.title}
-                </li>
-              ))}
-        </ul>
-        <div style={{ padding: "10px" }}>
-          <input
-            type="text"
-            onChange={e => this.setState({ title: e.target.value })}
-            placeholder="add title in the database"
-          />
-          <button onClick={() => this.putDataToDB(this.state.title)}>
-            ADD
-          </button>
-        </div>
-        <div >
-          <input
-            type="text"
-            
-            onChange={e => this.setState({ idToDelete: e.target.value })}
-            placeholder="put id of item to delete here"
-          />
-          <button onClick={() => this.deleteFromDB(this.state.idToDelete)}>
-            DELETE
-          </button>
-        </div>
-        <div >
-          <input
-            type="text"
-            onChange={e => this.setState({ idToUpdate: e.target.value })}
-            placeholder="id of item to update here"
-          />
-          <input
-            type="text"
-            onChange={e => this.setState({ updateToApply: e.target.value })}
-            placeholder="put new value of the item here"
-          />
-          <button
-            onClick={() =>
-              this.updateDB(this.state.idToUpdate, this.state.updateToApply)
-            }
-          >
-            UPDATE
-          </button>
-        </div>
-      </div>
-    );
-  }
+  )
 }
-export default FromResearchCard;
