87baabab4c53a675e7a14da03d9d9af0274f4411
martinrebo <martinrg33@gmail.com>
2019-04-13 17:47:44 +0200

    Fix FormResearchCard - Add Branch buttons

--- README.md
+++ README.md
@@ -19,6 +19,10 @@ https://www.videezy.com/backgrounds/5054-ceiling-flames-4k-motion-background-loo
 React componets
 https://bitsrc.io/
 
+Firebase get data 
+https://medium.com/@scarygami/cloud-firestore-quicktip-documentsnapshot-vs-querysnapshot-70aef6d57ab3
+
+
 Icons
 <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 
--- src/Components/FormResearchCard/FormResearchCard.js
+++ src/Components/FormResearchCard/FormResearchCard.js
@@ -7,8 +7,9 @@ export default function FormResearchCard() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [link, setLink] = useState("");
+  const [branch, setBranch] = useState("")
 
-  let data = { title, description, link };
+  let data = { title, description, link, branch };
 
   function sendData(e) {
     e.preventDefault();
@@ -16,18 +17,21 @@ export default function FormResearchCard() {
     fire.collection('card-research').add({
       title: data.title,
       description: data.description,
-      link: data.link
+      link: data.link,
+      branch: data.branch
     }).then(
       function (docRef) {
         console.log("Document written with ID: ", docRef.id);
-        alert( `You added a new card:
-        title: ${data.title } 
+        alert(`You added a new card:
+        title: ${data.title} 
         description: ${data.link}
         link: ${data.link}
+        branch: ${data.branch}
         `)
         setTitle("");
         setDescription("");
         setLink("");
+        setBranch("");
 
       }
     ).catch(
@@ -38,20 +42,48 @@ export default function FormResearchCard() {
   }
 
   return (
-    <form onSubmit={(e) => { sendData(e) }}>
-      <input type="text" placeholder="Title" value={title} onChange={(e) => {
-        e.preventDefault();
-        setTitle(e.target.value)
-      }
-      } />
-      <input type="text" placeholder="Description" value={description} onChange={(e) => {
-        e.preventDefault();
-        setDescription(e.target.value)
-      }} />
-      <input type="text" placeholder="Link" value={link} onChange={(e) => {
-        e.preventDefault();
-        setLink(e.target.value)
-      }} />
+    <form  onSubmit={(e) => { sendData(e) }}>
+      <input className="inputForm" type="text" placeholder="Title" required minLength="3" maxLength="140"
+        value={title} onChange={(e) => {
+          e.preventDefault();
+          setTitle(e.target.value)
+        }
+        } />
+      <input className="inputForm" type="text" placeholder="Description" required minLength="30" maxLength="300"
+        value={description} onChange={(e) => {
+          e.preventDefault();
+          setDescription(e.target.value)
+        }} />
+      <input className="inputForm" type="url" placeholder="https://example.com" required pattern="https?://.+"
+        value={link} onChange={(e) => {
+          e.preventDefault();
+          setLink(e.target.value)
+        }} />
+
+
+      <label>
+        <input  className="inputRadio" type="radio" value="business" checked={branch === "business"}
+          onChange={(e) => {
+            setBranch(e.target.value);
+           
+          }} /> Business
+      </label>
+      <label>
+        <input className="inputRadio" type="radio" value="tech" checked={branch === "tech"}
+          onChange={(e) => {
+            setBranch(e.target.value);
+            
+          }} /> Tech
+      </label>
+      <label>
+        <input className="inputRadio" type="radio" value="health" checked={branch === "health"}
+          onChange={(e) => {
+            setBranch(e.target.value);
+            
+          }} /> Health
+</label>
+
+
       <button type="submit" className="submit"> Submit Card </button>
     </form>
 
